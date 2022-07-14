c2cb418346e8f7b1ecc929921a5b36f9fb91aedb
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-07-14 14:34:16 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import fetch from "node-fetch"
+const app = express();
+
+app.get("/get/:id", (req, res) => {
+    (async () => {  
+    var f = await fetch("https://pinball3d.github.io/storage/urlredir.json")
+    var data = await f.json();
+    try {
+    res.setHeader("location", data[req.params["id"]]);
+    res.sendStatus(301);
+    } catch(err) {
+        res.sendStatus(400);
+    }
+      })();
+})
+
+app.listen(process.env.port || 80);
